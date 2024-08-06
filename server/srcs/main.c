560e04ee33fc4d6f51d5a172e4f0543ed9172dcc
Ha ju Young <jy.h4456@arielnetworks.co.kr>
2024-08-06 09:41:05 +0900

    [Feat] nonblock 로직 추가, 헤더타입으로 echo 서버 변경

--- server/srcs/main.c
+++ server/srcs/main.c
@@ -11,6 +11,8 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 
 #define CLIENT_NUM  1025
 
@@ -21,52 +23,38 @@ typedef struct
 
 int recv_(t_client *client, int target_fd, fd_set *r_fd_set, fd_set *w_fd_set)
 {
-    int goal_len = BUFSIZ;
     int start_pos = 0;
-	int recv_len = recv(target_fd, client + target_fd, goal_len, 0);
+	int recv_len = 0;
 
-	if (0 > recv_len) 
-        return 0;
-	else if (recv_len == 0)
+	do
 	{
-        // client leave
-        bzero(client + target_fd, sizeof(t_client));
-		close(target_fd);
-		FD_CLR(target_fd, w_fd_set);
-		FD_CLR(target_fd, r_fd_set);
-	} 
-	else 
-	{
-		printf("goal_len : %d, recv_len : %d, start_pos : %d\n", goal_len, recv_len, start_pos);
-		goal_len -= recv_len;
-		while (0 < goal_len)
+		int recv_len = recv(target_fd, client + target_fd + start_pos, BUFSIZ, 0);
+		if (recv_len == 0)
 		{
-			recv_len = recv(target_fd, client[target_fd].buf + start_pos, goal_len, 0);
-			goal_len -= recv_len;
-			start_pos += recv_len;
+			// client leave
+			bzero(client + target_fd, sizeof(t_client));
+			close(target_fd);
+			FD_CLR(target_fd, w_fd_set);
+			FD_CLR(target_fd, r_fd_set);
+			return 1;
 		}
-		FD_SET(target_fd, w_fd_set);
-	}
+		start_pos += recv_len;
+	} while (recv_len == -1);
+
+	FD_SET(target_fd, w_fd_set);
 	return 1;
 }
 
 void send_(t_client *client, int target_fd, fd_set *w_fd_set)
 {
-    int goal_len = BUFSIZ;
     int send_len = 0;
     int start_pos = 0;
 
     do 
     {
-        send_len = send(target_fd , client + target_fd + start_pos, goal_len, 0);
-		if (send_len == -1)
-		{
-			fprintf(stderr, "send Error: %s\n", strerror(errno));
-			break;
-		}
+        send_len = send(target_fd , client + target_fd + start_pos, BUFSIZ - start_pos, 0);
 		start_pos += send_len;
-        goal_len -= send_len;
-    } while (goal_len > 0);
+    } while (send_len == -1);
 
     // 해당 클라이언트 초기화
     bzero(client + target_fd, sizeof(t_client));
@@ -81,6 +69,17 @@ void check_argc(int argc)
         ari_title_print_fd(STDERR_FILENO, "ex: [4242]", COLOR_RED_CODE);
         exit(EXIT_FAILURE);
     }
+}
+
+int set_non_blocking(int sockfd)
+{
+	int flags = fcntl(sockfd, F_GETFL, 0);
+
+	if (flags == -1) {
+        perror("fcntl get");
+        return -1;
+    }
+    return fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 }
 
 int init_socket(char *port, struct sockaddr_in *servaddr)
@@ -106,14 +105,20 @@ int init_socket(char *port, struct sockaddr_in *servaddr)
 
 	// Binding newly created socket to given IP and verification 
 	if ((bind(sockfd, (const struct sockaddr *)servaddr, sizeof(*servaddr))) != 0) { 
-		ari_putstr_fd("socket bind failed...\n", 2); 
+		fprintf(stderr, "bind Error: %s\n", strerror(errno));
 		close(sockfd);
 		exit(1); 
 	} 
 	if (listen(sockfd, 10) != 0) {
-		ari_putstr_fd("cannot listen\n", 2); 
+		fprintf(stderr, "listen Error: %s\n", strerror(errno));
 		close(sockfd);
 		exit(1); 
+	}
+	if (set_non_blocking(sockfd) == -1)
+	{
+		fprintf(stderr, "nonblock Error: %s\n", strerror(errno));
+		close(sockfd);
+		exit(1);
 	}
     return sockfd;
 }
@@ -129,7 +134,6 @@ int main(int argc, char **argv) {
 	int select_cnt;
     int fd_max;
 
-	int id = 0;
     int len = sizeof(cli);
 
 	t_client client[CLIENT_NUM] = {0,};
@@ -155,16 +159,12 @@ int main(int argc, char **argv) {
 		select_cnt = select(fd_max + 1, &r_copy_fd_set, &w_copy_fd_set, NULL, NULL);
 		for(int target_fd = 3; target_fd <= fd_max && select_cnt > 0; target_fd++)
 		{
-            if (FD_ISSET(target_fd, &w_copy_fd_set))
-			{
-				send_(client, target_fd, &w_fd_set);
-                select_cnt--;
-            }
 			if (FD_ISSET(target_fd, &r_copy_fd_set))
 			{
 				if (target_fd != sockfd)
 				{
 					printf("in target_fd : %d\n", target_fd);
+
 					if (!recv_(client, target_fd, &r_fd_set, &w_fd_set))
 					{
 						for (int i = 3; i <= fd_max; i++)
@@ -190,11 +190,16 @@ int main(int argc, char **argv) {
 					if (connfd < 0) 
 						continue;
                     FD_SET(connfd, &r_fd_set);
+					set_non_blocking(connfd);
 					fprintf(stderr, "connfd : %d\n", connfd);
                     if (fd_max < connfd)
 						fd_max = connfd;
-					id++;
 				}
+                select_cnt--;
+            }
+            else if (FD_ISSET(target_fd, &w_copy_fd_set))
+			{
+				send_(client, target_fd, &w_fd_set);
                 select_cnt--;
             }
         }
