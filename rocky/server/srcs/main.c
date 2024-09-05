4bd4b207cc37623ada39fbab60142a889f35bcdc
Ha Ju Young <jy.h4456@arielnetworks.co.kr>
2024-09-05 16:48:48 +0900

    TEST:code

--- rocky/server/srcs/main.c
+++ rocky/server/srcs/main.c
@@ -10,6 +10,7 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
+#include <ulpLibInterface.h>
 
 #include "json-c/json.h"
 #include "libari.h"
@@ -74,6 +75,58 @@ typedef struct {
     char *buf_ptr;
     char buf[BUFSIZ];
 } t_client;
+
+static short _esqlopts[10] = {0,0,1,0,0,0,0,0,0,0};
+
+int db_connect() {
+	
+	char* usr="EMG";
+	char* pwd="EMG123";
+	char* conn_opt="Server=172.17.0.2;CONNTYPE=1";
+	
+
+	printf("<Try DB CONNECT>\n");
+
+{
+    struct ulpSqlstmt ulpSqlstmt;
+    memset(&ulpSqlstmt, 0, sizeof(ulpSqlstmt));
+    ulpHostVar ulpHostVar[3];
+    ulpSqlstmt.hostvalue = ulpHostVar;
+    ulpSqlstmt.stmttype = 1;
+    ulpSqlstmt.stmtname = NULL;
+    ulpSqlstmt.ismt = 0;
+    ulpSqlstmt.sqlinfo = 0;
+    ulpSqlstmt.numofhostvar = 3;
+    ulpSqlstmt.esqlopts    = _esqlopts;
+    ulpSqlstmt.sqlcaerr    = &sqlca;
+    ulpSqlstmt.sqlcodeerr  = &SQLCODE;
+    ulpSqlstmt.sqlstateerr = ulpGetSqlstate();
+    ulpSqlstmt.hostvalue[0].mHostVar = (void*)usr;
+    ulpSqlstmt.hostvalue[1].mHostVar = (void*)pwd;
+    ulpSqlstmt.hostvalue[2].mHostVar = (void*)conn_opt;
+    ulpDoEmsql( NULL, &ulpSqlstmt, NULL );
+}
+	return (sqlca.sqlcode == SQL_SUCCESS);
+}
+
+int db_disconnect(){
+	/*  DISCONNECT; */
+{
+    struct ulpSqlstmt ulpSqlstmt;
+    memset(&ulpSqlstmt, 0, sizeof(ulpSqlstmt));
+    ulpSqlstmt.stmttype = 2;
+    ulpSqlstmt.stmtname = NULL;
+    ulpSqlstmt.ismt = 0;
+    ulpSqlstmt.numofhostvar = 0;
+    ulpSqlstmt.esqlopts    = _esqlopts;
+    ulpSqlstmt.sqlcaerr    = &sqlca;
+    ulpSqlstmt.sqlcodeerr  = &SQLCODE;
+    ulpSqlstmt.sqlstateerr = ulpGetSqlstate();
+    ulpDoEmsql( NULL, &ulpSqlstmt, NULL );
+}
+
+	return (sqlca.sqlcode == SQL_SUCCESS);
+}
 
 int same_as_akey(RestLibHeadType *rest_msg) {
     static char *akey = "EMG@an#2345&12980!";
@@ -467,6 +520,11 @@ int main(int argc, char **argv) {
     check_argc(argc);
 
     sockfd = init_socket(argv[1], &servaddr);
+
+// TODO: test
+    printf("db : %d\n", db_connect());
+    exit(1);
+    db_disconnect();
 
     fd_set w_fd_set;
     fd_set r_fd_set;
