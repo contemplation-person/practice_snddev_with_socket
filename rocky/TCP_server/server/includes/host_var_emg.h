fd2b9f126e520f468c17b57cd99e76f09cdcb0d7
Ha Ju Young <jy.h4456@arielnetworks.co.kr>
2024-09-19 17:14:48 +0900

    [Bug] altibase 규격상 struct element 순서가 바뀌면 안됨.

--- rocky/TCP_server/server/includes/host_var_emg.h
+++ rocky/TCP_server/server/includes/host_var_emg.h
@@ -1,22 +1,18 @@
 EXEC SQL BEGIN DECLARE SECTION;
 
 typedef struct {
-    char LTEID[21];       // 기업 ID
-    char SLICE_ID[5];     // 슬라이스 ID
-    char DEVICE_ID[18];   // 2차 단말의 DEVICE_ID
-    char MDN[12];         // 1차 단말의 MDN
-    char IP[40];          // 사용 IP
-    char USER_ID[23];     // 사용자 ID
-    char DEVICE_TYPE[11]; // 단말 타입 (PC, CCTV, PRINTER, ETC)
-    char MODEL_NAME[31];  // 모델명
-    char SERIAL_NUM[31];  // 시리얼 번호
-
-    int AUTH_TYPE;        // 인증 타입 (0: EAP 인증, 1: EAP 미인증)
-    int TWO_FA_USE;       // 2-factor 인증 사용 여부
-    int DEVICE_SUSPEND;   // 단말 일시정지 여부
-    int ID_TYPE;          // ID 타입 (1: MAC, 2: CPU serial, 3: 추후 정의)
-    int IP_POOL_INDEX;    // IP Pool Index
+    char LTEID[22];         // VARCHAR(21) + 1
+    char SLICE_ID[6];       // VARCHAR(5) + 1
+    char MDN[13];           // VARCHAR(12) + 1
+    int  IP_POOL_INDEX;     // INTEGER
+    char IP[41];            // VARCHAR(40) + 1
+    int  AUTH_TYPE;         // INTEGER
+    char USER_ID[24];       // VARCHAR(23) + 1
+    int  TWOFA_USE;         // INTEGER
+    int  DEVICE_SUSPEND;    // INTEGER
+    char DEVICE_TYPE[12];   // VARCHAR(11) + 1
+    char MODEL_NAME[32];    // VARCHAR(31) + 1
+    char SERIAL_NUM[32];    // VARCHAR(31) + 1
 } Emg_type;
 
 EXEC SQL END DECLARE SECTION;
-
